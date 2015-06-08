7ed027219940d557aa55a7fd48153161aca157a2
Lia Skalkos <hello@liaskalkos.net>
2015-06-08 15:31:27 -0400

    Working

--- ruby_phone_format.rb
+++ ruby_phone_format.rb
@@ -1,1 +1,14 @@
 # Write your code here
+def normalize_phone_number(phone_number)
+
+  # pretty_number = phone_number.scan(/\d/).join
+
+  pretty_number = phone_number.scan(/\d/).join
+
+  if pretty_number.length == 10
+    pretty_number.insert(6, "-").insert(3,") ").insert(0, "(")
+  else
+    phone_number
+  end
+
+end
